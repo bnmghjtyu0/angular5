f6cdb57d696d4a15d69ba573a42a7a4b4431a61b
bnmghjtyu0 <bnmghjtyu0@gmail.com>
2017-11-19 15:31:30 +0800

    08 練習範本參考變數使用

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,14 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'app';
-  searchText = '';
-  changeSearchText($event){
-    console.log($event);
-    this.searchText = $event
+  title = "app";
+  searchText = "";
+  changeSearchText(value) {
+    this.searchText = value;
   }
 }
