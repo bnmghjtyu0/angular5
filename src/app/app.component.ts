75577cc381be36c18ae6ea1ca43c9c570525c02a
bnmghjtyu0 <bnmghjtyu0@gmail.com>
2017-11-19 15:15:59 +0800

    07 練習將雙向繫結的行為改成單向繫結

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app';
   searchText = '';
+  changeSearchText($event){
+    console.log($event);
+    this.searchText = $event
+  }
 }
