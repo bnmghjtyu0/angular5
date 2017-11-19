b9976da7a23d56ba6bd9a64c82d8e600bdb489dc
bnmghjtyu0 <bnmghjtyu0@gmail.com>
2017-11-19 14:22:16 +0800

    04 練習 Angular 內嵌繫結與屬性繫結

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.css"]
 })
 export class HeaderComponent implements OnInit {
+  num:number = 1;
+  title: string = "記載著 Will 在網路世界的學習心得與技術分享";
+  titleURL: string = "http://blog.miniasp.com/";
 
-  constructor() { }
-
+  constructor() {}
   ngOnInit() {
   }
-
 }
