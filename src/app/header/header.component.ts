6c5d0572c7bce7a619df244177ce4483c4578f88
bnmghjtyu0 <bnmghjtyu0@gmail.com>
2017-11-20 21:57:34 +0800

    09-1 練習 Attribute Directive (屬性型指令) 用法: ngStyle

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -9,11 +9,12 @@ export class HeaderComponent implements OnInit {
   num:number = 1;
   title: string = "記載著 Will 在網路世界的學習心得與技術分享";
   titleURL: string = "http://blog.miniasp.com/";
-
+  titleSize:number = 32;
   constructor() {}
   ngOnInit() {
   }
   changeTitle(){
-    this.title += ' !'
+    this.title += ' !';
+    this.titleSize ++;
   }
 }
