533d20ca4d540573c511520e1365e9dfc3b78466
bnmghjtyu0 <bnmghjtyu0@gmail.com>
2017-11-20 22:37:12 +0800

    11 練習 Angular 的 @Input() 將資料從父元件傳值到子元件中

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { ArticleComponent } from './article/article.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ArticleComponent
   ],
   imports: [
     BrowserModule,
