0a886a22940fabf916a9b0d66ef8d7170a259c3e
Richard <bnmghjtyu0@gmail.com>
2017-11-21 14:36:18 +0800

    12-1 Angular相依注入-服務元件

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { FormsModule } from '@angular/forms';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { ArticleComponent } from './article/article.component';
-
+import { AppRoutingModule } from "./app-routing.module";
+import { FormsModule } from "@angular/forms";
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+import { ArticleComponent } from "./article/article.component";
+import { DataService } from "./data.service";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    ArticleComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule
-  ],
-  providers: [],
+  declarations: [AppComponent, HeaderComponent, ArticleComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
